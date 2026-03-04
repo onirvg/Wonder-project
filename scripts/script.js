2538d925483bf3966880048c36862de19b9a2578
onirvg <onirvg@gmail.com>
2026-03-04 21:19:19 +0300

    modify: обновлены радиобатоны, поправил календарь

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,27 @@
 import { createCalendar as generateCalendar, getDates } from './modules/calendar.js'
 import { getPassengersCounter} from './modules/addPassengers.js'
 
+
+
 generateCalendar()
+
+
+const roundTrip = document.getElementById('round-trip');
+const oneWayTrip = document.getElementById('one-way')
+const backDate = document.getElementById('back-day')
+
+function updateTrip() {
+if (oneWayTrip.checked) {
+    backDate.classList.add("hidden")
+}
+else{
+    backDate.classList.remove("hidden")
+}
+}
+
+roundTrip.addEventListener("change",updateTrip)
+oneWayTrip.addEventListener("change",updateTrip)
+
+updateTrip()
+
+
